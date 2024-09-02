e5d0c0abe607ea5b7f518e61e360f63584b4a5fe
Gerson Carneiro <gerson.tpc@outlook.com>
2024-09-02 02:55:05 -0300

    chore: refactor tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,11 +1,11 @@
 output "mamager_public_ip" {
-  value = ["${aws_instance.manager.*.public_ip}"]
+  value = aws_instance.manager[0].public_ip
 }
 
 output "mamager_private_ip" {
-  value = ["${aws_instance.manager.*.private_ip}"]
+  value = aws_instance.manager[0].private_ip
 }
 
 output "nodes_private_ip" {
-  value = ["${aws_instance.nodes.*.private_ip}"]
+  value = aws_instance.nodes[0].private_ip
 }
