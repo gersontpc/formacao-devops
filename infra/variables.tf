2666e49f02be3966267e8cf78f83e38a3fa3be50
Gerson Carneiro <gerson.tpc@outlook.com>
2024-09-02 03:03:16 -0300

    chore: refactor tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,11 +1,13 @@
 variable "aws_region" {
   description = "AWS region on which we will setup the swarm cluster"
   default     = "us-east-1"
+  type        = string
 }
 
 variable "instance_type" {
   description = "Instance type"
   default     = "t2.micro"
+  type        = string
 }
 
 variable "vpc_id" {
